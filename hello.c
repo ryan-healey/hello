8d9cfd23a55f7b62198094ad772dfbd4a52ac1fb
Ayden-Uerling <59975454+Ayden-Uerling@users.noreply.github.com>
2020-01-16 13:32:15 -0600

    Update hello.c
    Added my name and major to it

--- hello.c
+++ hello.c
@@ -3,6 +3,8 @@
 
 int main(void) {
 	printf("Ryan Healey\n");
-	printf("Electrical Engineering\n")
+	printf("Electrical Engineering\n");
+	printf("Ayden uerling\n");
+	printf("Electrical Engineering\n");
 	return 0;
 }
